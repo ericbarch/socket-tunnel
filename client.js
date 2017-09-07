f80c56ab5fc8de50c8e695482835b21a273bcdef
Igor Perevozchikov <iperevozchikov@gmail.com>
2017-09-07 11:46:08 +0700

    Refactor client to use options instead of hardcoded variables

--- client.js
+++ client.js
@@ -1,36 +1,33 @@
-// CONFIG
-var REQUESTED_SUBDOMAIN = 'myname';
-var TUNNEL_SERVER = 'http://mydomain.com';
-var LOCAL_PORT = 80;
+module.exports = function (options) {
+    // require the things we need
+    var net = require('net');
+    var ss = require('socket.io-stream');
+    var socket = require('socket.io-client')(options['tunnel-server']);
 
-// require the things we need
-var net = require('net');
-var ss = require('socket.io-stream');
-var socket = require('socket.io-client')(TUNNEL_SERVER);
+    socket.on('connect', function () {
+        console.log(new Date() + ': connected');
+        console.log(new Date() + ': requesting subdomain ' + options['subdomain'] + ' via ' + options['server']);
 
-socket.on('connect', function () {
-  console.log(new Date() + ': connected');
-  console.log(new Date() + ': requesting subdomain ' + REQUESTED_SUBDOMAIN + ' via ' + TUNNEL_SERVER);
-
-  socket.emit('createTunnel', REQUESTED_SUBDOMAIN);
-});
-
-socket.on('incomingClient', function (clientId) {
-  var client = net.connect(LOCAL_PORT, '127.0.0.1', function () {
-    var s = ss.createStream();
-    s.pipe(client).pipe(s);
-
-    s.on('end', function () {
-      client.destroy();
+        socket.emit('createTunnel', options['subdomain']);
     });
 
-    ss(socket).emit(clientId, s);
-  });
+    socket.on('incomingClient', function (clientId) {
+        var client = net.connect(options['port'], options['hostname'], function () {
+            var s = ss.createStream();
+            s.pipe(client).pipe(s);
 
-  client.on('error', function () {
-    // handle connection refusal
-    var s = ss.createStream();
-    ss(socket).emit(clientId, s);
-    s.end();
-  });
-});
+            s.on('end', function () {
+                client.destroy();
+            });
+
+            ss(socket).emit(clientId, s);
+        });
+
+        client.on('error', function () {
+            // handle connection refusal
+            var s = ss.createStream();
+            ss(socket).emit(clientId, s);
+            s.end();
+        });
+    });
+};
